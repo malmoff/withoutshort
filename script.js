c03d1e65b713e7dec6c5e13b93a07f5b9877d8aa
malmoff <81243022+malmoff@users.noreply.github.com>
2023-07-27 23:32:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -45,36 +45,7 @@ function Search(query) {
     });
 }
 
-// Funktion för att visa/dölja rullgardinen på mobila enheter
-function toggleMobileMenu() {
-  const mobileMenu = document.querySelector(".mobile");
-  mobileMenu.classList.toggle("hidden");
-}
-
-// Klickhändelse för att visa/dölja rullgardinen på mobila enheter
-const menuBtn = document.querySelector(".menuBtn");
-menuBtn.addEventListener("click", toggleMobileMenu);
-
-// Kategorilänkar för mobila enheter
-const mobileCategories = document.querySelectorAll(".mobile nav li");
-mobileCategories.forEach((category) => {
-  category.addEventListener("click", () => {
-    toggleMobileMenu(); // Dölj rullgardinen efter att en kategori har valts
-    const query = category.textContent.toLowerCase();
-    Search(query);
-  });
-});
-
-// Sökruta för mobila enheter
-const searchFormMobile = document.getElementById("searchFormMobile");
-searchFormMobile.addEventListener("submit", (e) => {
-  e.preventDefault();
-  toggleMobileMenu(); // Dölj rullgardinen efter att sökningen har skickats
-  const query = document.getElementById("searchInputMobile").value;
-  Search(query);
-});
-
-// Kategorilänkar för desktop
+// Kategorilänkar
 const categories = document.querySelectorAll("nav.desktop li");
 categories.forEach((category) => {
   category.addEventListener("click", () => {
@@ -83,10 +54,26 @@ categories.forEach((category) => {
   });
 });
 
-// Sökruta för desktop
+// Sökruta
 const searchForm = document.getElementById("searchForm");
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const query = document.getElementById("searchInput").value;
   Search(query);
+});
+
+// Sökruta för mobil
+const searchFormMobile = document.getElementById("searchFormMobile");
+searchFormMobile.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const query = document.getElementById("searchInputMobile").value;
+  Search(query);
+});
+
+// Menyknapp för mobil
+const menuBtn = document.querySelector(".menuBtn");
+const mobileMenu = document.querySelector(".mobile");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
 });
